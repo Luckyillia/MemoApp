439bf61d83a970303269b2214fb0cab8e187612f
Illia Zaichenko <10376901u@qed.local>
2024-10-17 13:48:08 +0200

    Add reference to obiekt ImageView

--- app/src/main/java/com/example/memoapp/MainActivity.java
+++ app/src/main/java/com/example/memoapp/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.memoapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int[] tab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        ImageView karta1 = findViewById(R.id.karta1);
+        ImageView karta2 = findViewById(R.id.karta2);
+        ImageView karta3 = findViewById(R.id.karta3);
+        ImageView karta4 = findViewById(R.id.karta4);
+        ImageView karta5 = findViewById(R.id.karta5);
+        ImageView karta6 = findViewById(R.id.karta6);
+        ImageView karta7 = findViewById(R.id.karta7);
+        ImageView karta8 = findViewById(R.id.karta8);
+        ImageView karta9 = findViewById(R.id.karta9);
+        ImageView karta10 = findViewById(R.id.karta10);
+        ImageView karta11 = findViewById(R.id.karta11);
+        ImageView karta12 = findViewById(R.id.karta12);
+        ImageView karta13 = findViewById(R.id.karta13);
+        ImageView karta14 = findViewById(R.id.karta14);
+        ImageView karta15 = findViewById(R.id.karta15);
+        ImageView karta16 = findViewById(R.id.karta16);
+
     }
 }

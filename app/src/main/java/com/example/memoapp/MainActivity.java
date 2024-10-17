588c21a4a243e118acb187a39588944f34199172
Illia Zaichenko <10376901u@qed.local>
2024-10-17 15:47:29 +0200

    add listener on button

--- app/src/main/java/com/example/memoapp/MainActivity.java
+++ app/src/main/java/com/example/memoapp/MainActivity.java
@@ -1,36 +1,44 @@
 package com.example.memoapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.ImageView;
-
+import android.widget.ImageButton;
+import java.util.ArrayList;
+import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private int[] tab;
+    private List<ImageButton> buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        ImageView karta1 = findViewById(R.id.karta1);
-        ImageView karta2 = findViewById(R.id.karta2);
-        ImageView karta3 = findViewById(R.id.karta3);
-        ImageView karta4 = findViewById(R.id.karta4);
-        ImageView karta5 = findViewById(R.id.karta5);
-        ImageView karta6 = findViewById(R.id.karta6);
-        ImageView karta7 = findViewById(R.id.karta7);
-        ImageView karta8 = findViewById(R.id.karta8);
-        ImageView karta9 = findViewById(R.id.karta9);
-        ImageView karta10 = findViewById(R.id.karta10);
-        ImageView karta11 = findViewById(R.id.karta11);
-        ImageView karta12 = findViewById(R.id.karta12);
-        ImageView karta13 = findViewById(R.id.karta13);
-        ImageView karta14 = findViewById(R.id.karta14);
-        ImageView karta15 = findViewById(R.id.karta15);
-        ImageView karta16 = findViewById(R.id.karta16);
+        Button butto = findViewById(R.id.button);
+        buttons = new ArrayList<>();
+        buttons.add(findViewById(R.id.karta1));
+        buttons.add(findViewById(R.id.karta2));
+        buttons.add(findViewById(R.id.karta3));
+        buttons.add(findViewById(R.id.karta4));
+        buttons.add(findViewById(R.id.karta5));
+        buttons.add(findViewById(R.id.karta6));
+        buttons.add(findViewById(R.id.karta7));
+        buttons.add(findViewById(R.id.karta8));
+        buttons.add(findViewById(R.id.karta9));
+        buttons.add(findViewById(R.id.karta10));
+        buttons.add(findViewById(R.id.karta11));
+        buttons.add(findViewById(R.id.karta12));
+        buttons.add(findViewById(R.id.karta13));
+        buttons.add(findViewById(R.id.karta14));
+        buttons.add(findViewById(R.id.karta15));
+        buttons.add(findViewById(R.id.karta16));
 
+        for (ImageButton button : buttons) {
+            button.setOnClickListener(v -> Log.i("button", "Clicked: " + button));
+            button.setEnabled(false);
+        }
     }
 }
+
